ef1da4b75dcc64f53b0418c1f4810ec5e874bf6a
LudwigDobner <122929929+Blackiups@users.noreply.github.com>
2023-10-30 16:14:05 +0100

    Update loginmask.php

--- loginmask.php
+++ loginmask.php
@@ -7,12 +7,12 @@
     <h2>Anmelden</h2>
     <form action="login.php" method="post">
         <label for="benutzername">Benutzername:</label>
-        <input type="text" name="benutzername" id="benutzername" required><br><br>
+        <input type="text" name="benutzername" id="benutzername" required></input>
 
         <label for="passwort">Passwort:</label>
-        <input type="password" name="passwort" id="passwort" required><br><br>
+        <input type="password" name="passwort" id="passwort" required></input>
 
-        <input type="submit" value="Anmelden">
+        <input type="submit" value="Anmelden"></input>
     </form>
 </body>
 </html>
