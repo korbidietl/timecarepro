beb9584b9ef8b794fd3f10c7e8bb78288d2e8848
dobner02 <dobner02@ads.uni-passau.de>
2023-11-26 13:39:12 +0100

    Edit Config File for DB Connection

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_NAME', 'timecarepro');
 define('DB_USER', 'grup3sql');
 define('DB_PASSWORD', 'skill59UP86VM');
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db');
 
 $pdo = new PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
