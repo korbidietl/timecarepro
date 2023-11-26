e8deed866a82f1b581804e86a654b67f77800cdd
dobner02 <dobner02@ads.uni-passau.de>
2023-11-26 12:59:11 +0100

    Edit Config File for DB Connection

--- login.php
+++ login.php
@@ -1,17 +1,46 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $benutzername = $_POST["benutzername"];
-    $passwort = $_POST["passwort"];
+session_start();
+$pdo = new PDO('mysql:host=db;dbname=timecarepro', 'grup3sql', 'skill59UP86VM');
 
-    // Hier können Sie die Benutzerauthentifizierung implementieren
-    // Zum Beispiel, eine Datenbankabfrage, um Benutzerdaten zu überprüfen
+if(isset($_GET['login'])) {
+    $email = $_POST['email'];
+    $passwort = $_POST['passwort'];
 
-    // Wenn die Authentifizierung erfolgreich ist, leiten Sie den Benutzer auf eine Erfolgsseite weiter
-    if ($passwort/* Bedingung für erfolgreiche Anmeldung */) {
-        header("Location: Inteltest.html");
-        exit;
+    $statement = $pdo->prepare("SELECT * FROM person WHERE email = :email");
+    $result = $statement->execute(array('email' => $email));
+    $user = $statement->fetch();
+
+    //Überprüfung des Passworts
+    if ($user !== false && password_verify($passwort, $user['passwort'])) {
+        $_SESSION['userid'] = $user['id'];
+        die('Login erfolgreich. Weiter zu <a href="geheim.php">internen Bereich</a>');
     } else {
-        echo "Anmeldung fehlgeschlagen. Bitte überprüfen Sie Ihre Anmeldeinformationen.";
+        $errorMessage = "E-Mail oder Passwort war ungültig<br>";
     }
+
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<?php
+if(isset($errorMessage)) {
+    echo $errorMessage;
+}
+?>
+
+<form action="?login=1" method="post">
+    E-Mail:<br>
+    <input type="email" size="40" maxlength="250" name="email"><br><br>
+
+    Dein Passwort:<br>
+    <input type="password" size="40"  maxlength="250" name="passwort"><br>
+
+    <input type="submit" value="Abschicken">
+</form>
+</body>
+</html>
