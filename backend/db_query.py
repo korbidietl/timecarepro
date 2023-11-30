2c8cc22a4073af254b54d055c79edbe352242e4c
dobner02 <dobner02@ads.uni-passau.de>
2023-11-30 17:29:01 +0100

    Ludwig: update db_query.py & delete dbsql.py

--- backend/db_query.py
+++ backend/db_query.py
@@ -5,44 +5,60 @@ from passlib.utils import generate_password
 
 
 # Ludwig: @Korbi was das? xD
-def get_user_by_email(email):
+def get_user_by_email(self, email):
     connection = get_database_connection()
     cursor = connection.cursor(dictionary=True)  # Aktiviere dictionary-based Cursor
-    query = "SELECT * FROM person WHERE email = %s"
-    cursor.execute(query, (email,))
-    user = cursor.fetchone()  # Hole nur den ersten Treffer
+    cursor.execute("SELECT * FROM person WHERE email = %s", (email,))
+    user = cursor.fetchone()  # erstes Ergebnis wird aufgerufen
     cursor.close()
     connection.close()
     return user
-
 
 # Bitte anlegen: def get_password_for_user(email): -> rename: validate_login
 # Methode validate_login mit der E-Mail-Adresse und dem Passwort des Benutzers aufrufen.
 # Diese Methode gibt True zurück,
 # wenn die E-Mail-Adresse und das Passwort in der Datenbank vorhanden sind und korrekt sind.
 # Andernfalls gibt sie False zurück.
-def validate_login(email, password):
+def validate_login(self, email, password):
     connection = get_database_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT passwort FROM person WHERE email = %s", (email,))
-    result = cursor.fetchone()
+    result = cursor.fetchone()  # erstes Ergebnis wird aufgerufen
     if result:
         hashed_password = result[0]
         if sha1_crypt.verify(password, hashed_password):
             return True
     return False
 
-
 # Bitte anlegen: def get_role_for_user(email):
+# Methode, die die Rolle basierend auf der E-Mail aus der Datenbank abruft.
+# Wenn ein Ergebnis gefunden wird, wird es als Rolle (str) zurückgegeben. Andernfalls wird None zurückgegeben.
+def get_role_by_email(self, email):
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT rolle FROM person WHERE email = %s", (email,))
+    result = cursor.fetchone()  # erstes Ergebnis wird aufgerufen
+    cursor.close()
+    return result[0] if result else None
 
 # Bitte anlegen: def get_surnmae_for_user(email):
+# Methode gibt Vornamen zurück, wenn er in der Datenbank gefunden wird. Andernfalls gibt sie None zurück.
+def get_firstname_by_email(self, email):
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT vorname FROM person WHERE email = %s", (email,))
+    result = cursor.fetchone()  # erstes Ergebnis wird aufgerufen
+    if result:
+        return result[0]
+    else:
+        return None
 
 # Bitte anlegen: def set_password_for_user(password, email)
 # Passwort zurückzusetzen Methode reset_password mit der E-Mail-Adresse des Benutzers aufrufen.
 # Diese Methode generiert ein neues Passwort
 # hasht es mit SHA1 und speichert das gehashte Passwort in der Datenbank.
 # Anschließend wird das neue Passwort zurückgegeben.
-def reset_password(email):
+def reset_password(self, email):
     connection = get_database_connection()
     cursor = connection.cursor()
     new_password = generate_password(10)
@@ -52,10 +68,9 @@ def reset_password(email):
     connection.commit()
     return new_password
 
-
 # Überprüfung, ob eine Benutzer-ID für die gegebene E-Mail-Adresse existiert.
 # Wenn dies der Fall ist, gibt die Methode True zurück, sonst False.
-def validate_email(email):
+def validate_email(self, email):
     connection = get_database_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT id FROM person WHERE email = %s", (email,))
@@ -64,12 +79,11 @@ def validate_email(email):
         return True
     return False
 
-
 # Bitte anlegen: def get_locked_status(email):
 # Überprüfung, ob der Benutzer, der die gegebene E-Mail-Adresse hat, gesperrt ist.
 # Wenn der Wert des Feldes "sperre" 1 ist, gibt die Methode True zurück, was bedeutet,
 # dass das Benutzerkonto gesperrt ist. Andernfalls gibt die Methode False zurück.
-def check_account_locked(email):
+def check_account_locked(self, email):
     connection = get_database_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT sperre FROM person WHERE email = %s", (email,))
@@ -80,40 +94,7 @@ def check_account_locked(email):
     return False
 
 
-def fetch_data():
-    cnx = mysql.connector.connect(
-        host="localhost",
-        user="grup3sql",
-        password="skill59UP86VM",
-        database="timecarepro"
-    )
-    cursor = cnx.cursor()
-
-    query = """
-    SELECT
-        klient.ID, klient.nachname,
-        person.ID, person.nachname
-    FROM
-        klient
-    INNER JOIN
-        person ON klient.ID = person.ID
-    """
-    cursor.execute(query)
-
-    for (client_id, client_last_name, employee_id, employee_last_name) in cursor:
-        print("Client ID: {}, Client Last Name: {}, Employee ID: {}, Employee Last Name: {}".format(client_id,
-                                                                                                    client_last_name,
-                                                                                                    employee_id,
-                                                                                                    employee_last_name))
-
-    cursor.close()
-    cnx.close()
-
-
-if __name__ == "__main__":
-    fetch_data()
-
-
+# @korbi bitte anpassen/löschen/ hab ich glaub ich oben schon
 def update_password_for_user(email, new_password_hash):
     cnx = get_database_connection()
     cursor = cnx.cursor()
@@ -122,7 +103,6 @@ def update_password_for_user(email, new_password_hash):
     cnx.commit()
     cursor.close()
     cnx.close()
-
 
 def get_password_hash_for_user(email):
     cnx = get_database_connection()
