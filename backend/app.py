719ae1d9d33a96eedf088669d56b06e6b1c531f0
dobner02 <dobner02@ads.uni-passau.de>
2023-12-05 08:58:40 +0100

    Ludwig: app.py change und add requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,8 @@ from flask import Flask, session, render_template
 from login import einloggen_blueprint
 from password_reset import password_reset_blueprint
 from logout import logout_blueprint
-# from flask_session import Session
 from datetime import timedelta
 from middleware import before_request
-# from db_query import sachbearbeiter_dropdown
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
@@ -14,11 +12,15 @@ app.secret_key = "your_secret_key"
 app.config['SESSION_TYPE'] = 'filesystem'  # Du kannst 'filesystem' durch andere Optionen ersetzen
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)  # Setze die Inaktivitätszeit auf 30 Minuten
 
-
 # alle Blueprints
 app.register_blueprint(einloggen_blueprint)
 app.register_blueprint(password_reset_blueprint)
 app.register_blueprint(logout_blueprint)
+
+
+# @app.route("/")
+# def root():
+#     return "Coding Ninjas!!"
 
 
 session(app)
