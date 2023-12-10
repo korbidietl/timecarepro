400b9fb715848ebe8c9826a571f551ca60a7baf4
Stella Muratore <stella.muratore@gmx.at>
2023-12-10 11:40:10 +0100

    Stella: Betreuungsstunden Klienten einsehen Backend und Frontend

--- backend/show_supervisionhours_client.py
+++ backend/show_supervisionhours_client.py
@@ -1,9 +1,12 @@
-from flask import render_template
+from flask import Blueprint, render_template, request
 
-from backend.app import app
+client_hours_blueprint = Blueprint('client_hours_blueprint', __name__, template_folder='templates')
 
+@client_hours_blueprint.route('/client/<int:client_id>')
+def client_profile(client_id):
+    client_id = request.form.get('client_id')
+    client_name = get_client_name(client_id)
+    client_sachbearbeiter = get_sachbearbeiter_name(client_id)
 
-@app.route('/see_supervisionhours_client')
-def see_supervisionhours_client():
-    # Implementierung der Übersichtsansicht
-    pass
+    return render_template('client_profile.html', client_id=client_id)
+Reg
