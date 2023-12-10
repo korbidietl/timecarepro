d47800286d67fbee8450f5f755ec502047aaf1d1
Stella Muratore <stella.muratore@gmx.at>
2023-12-10 20:01:31 +0100

    Stella: Betreuungstunden Klienten einsehen Frontend und Backend

--- backend/show_supervisionhours_client.py
+++ backend/show_supervisionhours_client.py
@@ -5,7 +5,9 @@ import csv
 from flask import Blueprint, render_template, request, session, Response
 from datetime import datetime
 from db_query import get_client_name, get_sachbearbeiter_name, get_fallverantwortung_id, check_booked
+
 client_hours_blueprint = Blueprint('client_hours_blueprint', __name__, template_folder='templates')
+
 
 def generate_month_year_combinations():
     aktuelles_jahr = datetime.now().year
@@ -13,11 +15,12 @@ def generate_month_year_combinations():
               'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
     kombinationen = []
 
-    for jahr in range(aktuelles_jahr-5, aktuelles_jahr+1):
+    for jahr in range(aktuelles_jahr - 5, aktuelles_jahr + 1):
         for monat_index, monat in enumerate(monate, start=1):
             kombinationen.append(f"{monat} {jahr}")
 
     return kombinationen
+
 
 def extrahiere_jahr_und_monat(kombination):
     teile = kombination.split(' ')
@@ -31,15 +34,16 @@ def extrahiere_jahr_und_monat(kombination):
 
     return monat_nummer, int(jahr)
 
+
 @client_hours_blueprint.route('/client/<int:client_id>', methods='POST')
 def client_profile(client_id):
     kombinationen = generate_month_year_combinations()
 
-    #auswahl des angezeigten Zeitraums
-    if request.method =='POST':
+    # auswahl des angezeigten Zeitraums
+    if request.method == 'POST':
         gewaehlte_kombination = request.form.get('monat_jahr')
     else:
-        #Standardmäßig aktuelles Monat und Jahr
+        # Standardmäßig aktuelles Monat und Jahr
         monate = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
                   'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
         aktuelles_jahr = datetime.now().year
@@ -57,34 +61,32 @@ def client_profile(client_id):
     user_id = session.get('user_id')
     user_role = session.get('user_role')
 
-
-
     # Überprüfen ob Fallverantwortung hat
     if user_id == fallverantwortung_id:
         # Datenbankaufruf für alle anzeigen
         zeiteintraege_liste = get_zeiteintraege_for_client(client_id, monat, jahr)
-        return render_template('show_supervisionhours_client.html', zeiteintraege_liste=zeiteintraege_liste
+        return render_template('show_supervisionhours_client.html', zeiteintraege_liste=zeiteintraege_liste)
 
     else:
-        # Datenbankaufrug für Mitarbeiter_ID anzeigen
+        # Zeiteinträge nur von eigener ID (ohne Fallverantwortung)
+        zeiteintraege_liste_of = get_zeiteintraege_for_client_of(client_id)
+        return render_template('show_supervisionhours_client.html', zeiteintraege_liste=zeiteintraege_liste_of)
 
 
-
-    return render_template('show_supervisionhours_client.html', client_id=client_id, client_name = client_name,client_sachbearbeiter= client_sachbearbeiter)
-
-
-
+# Für Export
 def generiere_csv_daten(zeiteintraege_liste):
     output = io.StringIO()
     writer = csv.writer(output)
 
     # Beispiel: Schreiben der Kopfzeilen
-    writer.writerow(['Datum', 'Beschreibung', 'Kilometer','Anfang','Ende','Mitarbeitet','Unterschrift KLient', 'Unterschrift Mitarbeiter'])
+    writer.writerow(['Datum', 'Beschreibung', 'Kilometer', 'Anfang', 'Ende', 'Mitarbeitet', 'Unterschrift KLient',
+                     'Unterschrift Mitarbeiter'])
 
     # Schreiben der Datenzeilen
     for zeiteintrag in zeiteintraege_liste:
-        writer.writerow([zeiteintrag.datum, zeiteintrag.stunden, zeiteintrag.beschreibung, zeiteintrag.kilometer, zeiteintrag.anfang, zeiteintrag.ende, zeiteintrag.mitarbieter, zeiteintrag.unterschrift_klient, zeiteintrag.unterschrift_mitarbeiter])
-
+        writer.writerow([zeiteintrag.datum, zeiteintrag.stunden, zeiteintrag.beschreibung, zeiteintrag.kilometer,
+                         zeiteintrag.anfang, zeiteintrag.ende, zeiteintrag.mitarbieter, zeiteintrag.unterschrift_klient,
+                         zeiteintrag.unterschrift_mitarbeiter])
 
     return output.getvalue()
 
