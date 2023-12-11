dd389859c62c4dbe9b3c8fd6190bfd242261fb3b
Stella Muratore <stella.muratore@gmx.at>
2023-12-11 11:29:07 +0100

    Stella: Überarbeitung show_supervisionhours_client

--- backend/home.py
+++ backend/home.py
@@ -5,7 +5,7 @@ home_blueprint = Blueprint('home', __name__)
 
 @home_blueprint.route('/home', methods=['POST', 'GET'])
 def home():
-    if request.method('POST'):
+    if request.method == 'POST':
         return render_template('/home.html')
 
     return render_template('/home.html')
