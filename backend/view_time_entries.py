da63086125368d0834dab485c62b00b950b8931c
korbiniandietl <kd.munchen@gmail.com>
2023-12-12 11:57:59 +0100

    Korbi: edit view_time_entries.py

--- backend/view_time_entries.py
+++ backend/view_time_entries.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
-from db_query import get_person_by_id, get_time_entries_by_person_id, get_fahrten_by_zeiteintrag_id, check_booked
+from db_query import get_person_by_id, get_zeiteintrag_with_fahrten_by_id, check_booked
 
 view_time_entries_blueprint = Blueprint("view_time_entries", __name__, template_folder='templates')
 
@@ -8,9 +8,9 @@ def view_time_entries(person_id):
     person = get_person_by_id(person_id)
     if request.method == 'POST':
         month = request.form['month']
-        time_entries = get_time_entries_by_person_id(person_id, month)
+        time_entries = get_zeiteintrag_with_fahrten_by_id(person_id, month)
         for entry in time_entries:
-            entry['kilometer'] = sum([fahrt['kilometer'] for fahrt in get_fahrten_by_zeiteintrag_id(entry['id'])])
+            entry['kilometer'] = sum([fahrt['kilometer'] for fahrt in get_zeiteintrag_with_fahrten_by_id(entry['id'])])
             entry['buchung_vorhanden'] = check_booked(entry['id'])
         return render_template('view_time_entries.html', person=person, time_entries=time_entries)
     else:
