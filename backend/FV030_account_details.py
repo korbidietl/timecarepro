9022f4506ef21f728fa1edb149dcc4191cb09333
Stella Muratore <stella.muratore@gmx.at>
2023-12-13 11:20:51 +0100

    Stella: Backend und Frontend account_details

--- backend/FV030_account_details.py
+++ backend/FV030_account_details.py
@@ -3,11 +3,15 @@ from db_query import get_firstname_by_email, get_lastname_by_email
 
 account_details_blueprint = Blueprint('account_details', __name__)
 
-account_details_blueprint.route('/account_details', methods=['POST', 'GET'])
-def account_details(user_id):
-    if request.method == 'POST':
-        user_name =
 
+@account_details_blueprint.route('/account_details/<int:person_id>', methods=['POST', 'GET'])
+def account_details(person_id):
 
+    # Datenbankaufruf über person_id
+    # Datenbankaufruf ob gesperrt
+    # Datenbankaufruf nach role
+    locked = True
+    role = 'Mitarbeiter'
+    user_name = 'Hallo'
 
-    return render_template('FV030_account_details.py', user_id=user_id)
+    return render_template('account_details.html', person_id=person_id, locked=locked, role=role)
