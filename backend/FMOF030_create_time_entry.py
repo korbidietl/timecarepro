a49d9c9ffd75de1836050ffcb710e987599cacf0
korbiniandietl <kd.munchen@gmail.com>
2023-12-14 15:26:26 +0100

    Korbi: edit FMOF030_create_time_entry.py

--- backend/FMOF030_create_time_entry.py
+++ backend/FMOF030_create_time_entry.py
@@ -12,7 +12,6 @@ def submit_arbeitsstunden():
     datum = request.form.get('datum')
     start_zeit = request.form.get('startZeit')
     end_zeit = request.form.get('endZeit')
-    kilometer = request.form.get('kilometer')
     # da müssen wir uns noch überlegen, wie das am besten sinn macht
     # weil klient name kann ja doppelt sein, aber das dropdown soll ja keine id anzeigen
     # wie erkennt man aber im dropdown welcher max mustermann der richtige ist?
@@ -34,11 +33,19 @@ def submit_arbeitsstunden():
     # Füge neuen Zeiteintrag hinzu und erhalte die ID
     else:
         zeiteintrag_id = add_zeiteintrag(datum, start_datetime, end_datetime, beschreibung, interne_notiz,
-                                         unterschrift_klient, unterschrift_mitarbeiter)
+                                         unterschrift_klient, unterschrift_mitarbeiter, absage)
 
-        # Falls Kilometer angegeben, füge Fahrt hinzu
-        if kilometer:
-            add_fahrt(zeiteintrag_id, kilometer)
+        # Iteriere über alle Fahrt-Einträge und füge sie hinzu
+        fahrt_index = 0
+        while True:
+            abrechenbarkeit = request.form.get(f'abrechenbarkeit{fahrt_index}')
+            start_adresse = request.form.get(f'start_adresse{fahrt_index}')
+            end_adresse = request.form.get(f'end_adresse{fahrt_index}')
+            kilometer = request.form.get(f'kilometer{fahrt_index}')
+            if kilometer is None:
+                break  # Keine weiteren Fahrten im Formular
+            add_fahrt(zeiteintrag_id, abrechenbarkeit, start_adresse, end_adresse, kilometer)
+            fahrt_index += 1
 
         # prüft auf überschneidung einer bestehenden eintragung in der datenbank
         if check_for_overlapping_zeiteintrag(zeiteintrag_id, klient_id, start_zeit, end_zeit):
