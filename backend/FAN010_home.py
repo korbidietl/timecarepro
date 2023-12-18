70243586a6c81118578ef2fd43218dc6c147720a
Stella Muratore <stella.muratore@gmx.at>
2023-12-18 22:27:07 +0100

    Stella: Home

--- backend/FAN010_home.py
+++ backend/FAN010_home.py
@@ -1,11 +1,9 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, session
 
 home_blueprint = Blueprint('home', __name__)
 
 
 @home_blueprint.route('/home', methods=['POST', 'GET'])
 def home():
-    if request.method == 'POST':
-        return render_template('/FAN010_home.html')
 
     return render_template('/FAN010_home.html')
