7f1663ea66dfc4f5532e6c5ca42a11e2164951ae
Stella Muratore <stella.muratore@gmx.at>
2023-12-19 08:29:23 +0100

    Stella: Dropdown show_employee_tabel

--- backend/FAN030_show_employee_table.py
+++ backend/FAN030_show_employee_table.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from flask import render_template, request, session, Blueprint, flash
 from db_query import account_table, account_table_mitarbeiter, get_role_by_id
+from FMOF010_show_supervisionhours_client import generate_month_year_combinations, extrahiere_jahr_und_monat
 
 show_employee_table_blueprint = Blueprint('show_employee_table', __name__)
 
@@ -8,6 +11,21 @@ show_employee_table_blueprint = Blueprint('show_employee_table', __name__)
 def mitarbeiter():
     user_role = get_role_by_id(session.get('user_id'))
     mitarbeiterliste = []
+
+    kombinationen = generate_month_year_combinations()
+
+    # auswahl des angezeigten Zeitraums
+    if request.method == 'POST':
+        gewaehlte_kombination = request.form.get('monat_jahr')
+    else:
+        # Standardmäßig aktuelles Monat und Jahr
+        monate = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+                  'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
+        aktuelles_jahr = datetime.now().year
+        aktueller_monat = datetime.now().month
+        gewaehlte_kombination = f"{monate[aktueller_monat - 1]} {aktuelles_jahr}"
+
+    month, year = extrahiere_jahr_und_monat(gewaehlte_kombination)
 
     if request.method == 'POST':
 
@@ -24,9 +42,9 @@ def mitarbeiter():
     # Fehlerbehandlung, wenn keine Mitarbeiter gefunden werden
     if not mitarbeiterliste:
         flash("Keine Mitarbeiter gefunden.")
-        return render_template('FAN030_show_employee_table.html')
+        return render_template('FAN030_show_employee_table.html', kombinationen=kombinationen, gewaehlte_kombination=gewaehlte_kombination)
 
-    return render_template('FAN030_show_employee_table.html', mitarbeiterliste=mitarbeiterliste)
+    return render_template('FAN030_show_employee_table.html', mitarbeiterliste=mitarbeiterliste, kombinationen=kombinationen, gewaehlte_kombination=gewaehlte_kombination)
 
 
 
