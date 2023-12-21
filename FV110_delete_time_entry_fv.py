dd95f72dcd9faca94d5220f062799534bed6aec5
dobner02 <dobner02@ads.uni-passau.de>
2023-12-21 14:38:16 +0100

    Ludwig: edit FV110

--- FV110_delete_time_entry_fv.py
+++ FV110_delete_time_entry_fv.py
@@ -2,7 +2,7 @@ import smtplib
 from email.mime.text import MIMEText
 
 from flask import Blueprint, request, flash, redirect, url_for, render_template
-from db_query import check_booked, delete_zeiteintrag
+from db_query import check_booked, delete_zeiteintrag, get_email_by_zeiteintrag, get_lastname_by_email
 
 delete_time_entry_fv_blueprint = Blueprint("delete", __name__)
 
@@ -20,11 +20,10 @@ def send_email(email, subject, body):
     smtp.sendmail('deletetimeentry@timecarepro.de', [email], msg.as_string())
 
 
-def send_email_delete_time_entry(email, lastname):
-    subject = "Ihr neues Passwort"
+def send_email_delete_time_entry(email, lastname, id):
+    subject = "Gelöschter Zeiteintrag"
     body = (f"Sehr geehrte/r Frau/Mann {lastname}, \n\n"
-            f"Ihr Passwort wurde erfolgreich zurückgesetzt. "
-            f"Bitte ändern Sie dieses Passwort schnellstmöglich.\n\n"
+            f"Ihr Zeiteintrag {id} wurde von der Verwaltung gelöscht.\n\n"
             f"Mit freundlichen Grüßen\n"
             f"Ihr TimeCare Pro-Team")
     send_email(email, subject, body)
@@ -36,6 +35,8 @@ def delete_time_entry_fv(zeiteintrags_id):
         # übergebene ID und vermerk von welcher Funktion hierher geleitet
         origin_function = request.form.get('origin_function')
         booked = check_booked(zeiteintrags_id)
+        email = get_email_by_zeiteintrag(zeiteintrags_id)
+        lastname = get_lastname_by_email(email)
         # Zeiteintrag wurde schon gebucht
         if booked:
             error = ("Die Stundennachweise für diesen Monat wurden bereits gebucht."
@@ -46,7 +47,7 @@ def delete_time_entry_fv(zeiteintrags_id):
         else:
             # Löschen der Zeiteinträge und dazugehörigen Fahrten
             delete_zeiteintrag(zeiteintrags_id)
-
+            send_email_delete_time_entry(email, lastname, zeiteintrags_id)
             # Erfolgsmeldung
             success_message = "Eintrag erfolgreich gelöscht."
             flash(success_message, 'success')
