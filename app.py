3cc529a3856d62a54572a4bc27334380bfe467e6
Stella Muratore <stella.muratore@gmx.at>
2023-12-21 12:22:17 +0100

    Stella: Update Routen

--- app.py
+++ app.py
@@ -40,9 +40,15 @@ def create_app():
 
     # Sachbearbeiter/Kostenträger
 
+    from FSK010_access_hours_km_clients import access_hours_km_clients_blueprint
+    app.register_blueprint(access_hours_km_clients_blueprint)
+
     # Mitarbeiter ohne Fallverantwortung
     from FMOF010_show_supervisionhours_client import client_hours_blueprint
     app.register_blueprint(client_hours_blueprint)
+
+    from FMOF020_view_time_entries import view_time_entries_blueprint
+    app.register_blueprint(view_time_entries_blueprint)
 
     from FMOF030_create_time_entry import create_time_entry_blueprint
     app.register_blueprint(create_time_entry_blueprint)
