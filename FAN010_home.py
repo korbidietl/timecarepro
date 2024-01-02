e0643735375bfdc1ef0864262339918c69cf77ff
Stella Muratore <stella.muratore@gmx.at>
2024-01-02 22:51:09 +0100

    Stella: Überarbeitung Home für Mitarbeitertabelle anzeigen

--- FAN010_home.py
+++ FAN010_home.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, request, render_template, session
+from flask import Blueprint, render_template, session
 
 home_blueprint = Blueprint('home', __name__)
 
 
 @home_blueprint.route('/home', methods=['POST', 'GET'])
 def home():
+    user_id = session.get('user_id')
+    print(user_id)
 
-    return render_template('FAN010_home.html')
+    return render_template('FAN010_home.html', user_id=user_id)
