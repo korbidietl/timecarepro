812388f41f4f899773f702b90dc022729e2187a1
korbiniandietl <kd.munchen@gmail.com>
2024-01-05 14:42:03 +0100

    Korbi: überarbeitung FMOF020_view_time_entries.py

--- FMOF020_view_time_entries.py
+++ FMOF020_view_time_entries.py
@@ -8,6 +8,8 @@ view_time_entries_blueprint = Blueprint("view_time_entries", __name__)
 
 @view_time_entries_blueprint.route('/view_time_entries/<int:person_id>', methods=['GET', 'POST'])
 def view_time_entries(person_id):
+
+    # session daten speichern
     person = session.get('user_id')
     session['url'] = url_for('view_time_entries.view_time_entries', person_id=person_id)
 
@@ -15,32 +17,28 @@ def view_time_entries(person_id):
     kombinationen = generate_month_year_combinations()
     aktuelles_jahr = datetime.now().year
     aktueller_monat = datetime.now().month
+    monate = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+              'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
 
-    # auswahl des angezeigten Zeitraums
+    # Auswahl des angezeigten Zeitraums
     if request.method == 'POST':
         gewaehlte_kombination = request.form.get('monat_jahr')
     else:
         # Standardmäßig aktuelles Monat und Jahr
-        monate = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
-                  'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
-
         gewaehlte_kombination = f"{monate[aktueller_monat - 1]} {aktuelles_jahr}"
 
     month, year = extrahiere_jahr_und_monat(gewaehlte_kombination)
 
-    if request.method == 'POST':
+    # Abfragen der Zeiteinträge
+    zeiteintrag_ids = get_zeiteintrag_for_person(person_id, month, year)
+    time_entries = []
 
-        zeiteintrag_ids = get_zeiteintrag_for_person(person_id, month, year)
+    for zeiteintrag_id in zeiteintrag_ids:
+        zeiteintrag_with_fahrten = get_zeiteintrag_with_fahrten_by_id(zeiteintrag_id)
+        for entry in zeiteintrag_with_fahrten:
+            entry['zeiteintrag']['kilometer'] = sum([fahrt['kilometer'] for fahrt in entry['fahrten']])
+            entry['zeiteintrag']['buchung_vorhanden'] = check_booked(entry['zeiteintrag']['id'])
+            time_entries.append(entry['zeiteintrag'])
 
-        time_entries = []
+    return render_template('FMOF020_view_time_entries.html', person=person, time_entries=time_entries, kombinationen=kombinationen, gewaehlte_kombination=gewaehlte_kombination)
 
-        for zeiteintrag_id in zeiteintrag_ids:
-            zeiteintrag_with_fahrten = get_zeiteintrag_with_fahrten_by_id(zeiteintrag_id)
-            for entry in zeiteintrag_with_fahrten:
-                entry['zeiteintrag']['kilometer'] = sum([fahrt['kilometer'] for fahrt in entry['fahrten']])
-                entry['zeiteintrag']['buchung_vorhanden'] = check_booked(entry['zeiteintrag']['id'])
-                time_entries.append(entry['zeiteintrag'])
-
-        return render_template('FMOF020_view_time_entries.html', person=person, time_entries=time_entries, kombinationen=kombinationen, gewaehlte_kombination=gewaehlte_kombination)
-    else:
-        return render_template('FMOF020_view_time_entries.html', person=person, time_entries=[], kombinationen=kombinationen, gewaehlte_kombination=gewaehlte_kombination)
