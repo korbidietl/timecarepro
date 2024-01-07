2ad08a9cdee5c628af13c097eca042442e7f2a50
korbiniandietl <kd.munchen@gmail.com>
2024-01-07 01:18:01 +0100

    Korbi: überarbeitung

--- FV090_edit_client.py
+++ FV090_edit_client.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, request, jsonify, render_template, session, flash
+from flask import Blueprint, request, render_template, session, flash, redirect, url_for
 from db_query import edit_klient, mitarbeiter_dropdown, kostentraeger_dropdown, get_name_by_id, get_klient_data, get_current_client, get_new_client, save_change_log
 
 edit_client_blueprint = Blueprint('edit_client', __name__)
+
 
 @edit_client_blueprint.route('/edit_client/<int:client_id>', methods=['POST', 'GET'])
 def edit_client(client_id):
@@ -13,8 +14,7 @@ def edit_client(client_id):
     kostentraeger = kostentraeger_dropdown()
     fallverantwortung = mitarbeiter_dropdown()
 
-    if client_data_list and request.method == 'POST':
-        client_data = client_data_list[0]
+    if request.method == 'POST':
         vorname = request.form.get('vorname')
         nachname = request.form.get('nachname')
         geburtsdatum = request.form.get('geburtsdatum')
@@ -23,22 +23,23 @@ def edit_client(client_id):
         adresse = request.form.get('adresse')
         kontingent_hk = request.form.get('kontingent_hk')
         kontingent_fk = request.form.get('kontingent_fk')
-        fallverantwortung_id = request.form.get('ktDropdown')
-
-        print(fallverantwortung_id)
+        fallverantwortung_id = request.form.get('fvDropdown')
 
         try:
             edit_klient(client_id, vorname, nachname, geburtsdatum, telefonnummer, sachbearbeiter_id, adresse,
                         kontingent_hk, kontingent_fk, fallverantwortung_id)
-
+            print(21)
             new_client = get_new_client(client_id)
             save_change_log(person, "Klient", current_client, new_client)
-            flash(f'Klient {vorname} {nachname} wurde erfolgreich bearbeitet.')
-            # return jsonify({'message': 'Client successfully updated'}), 200
+            flash('Client successfully updated')
+            print("vor success")
+            return redirect(session.pop('url', None))
+            print("nach success")
         except Exception as e:
-            return jsonify({'message': 'Error updating client: ' + str(e)}), 500
+            flash('Error updating client: ' + str(e))
+            return redirect(url_for('edit_client.edit_client', client_id=client_id))
 
-    elif client_data_list:
+    else:
         client_data = client_data_list[0]
         firstname = client_data[1]
         lastname = client_data[2]
@@ -49,16 +50,17 @@ def edit_client(client_id):
         fk = client_data[7]
         hk = client_data[8]
         fv_id = client_data[9]
-
+        print(22)
         sachbearbeiter_data = get_name_by_id(sb_id)
         sachbearbeiter = sachbearbeiter_data[0] if sachbearbeiter_data else '-'
         fallverantwortung_data = get_name_by_id(fv_id)
         fallverantwortung_name = fallverantwortung_data[0] if fallverantwortung_data else '-'
-
+        print(23)
         return render_template('FV090_edit_client.html', kostentraeger=kostentraeger, fallverantwortung=fallverantwortung,
                                client_id=client_id, firstname=firstname, lastname=lastname, birthday=birthday,
                                phone=phone, sb=sachbearbeiter, address=address, fk=fk, hk=hk, fv=fallverantwortung_name)
 
     return render_template('FV090_edit_client.html', kostentraeger=kostentraeger, fallverantwortung=fallverantwortung, client_id=client_id)
+
 
 
