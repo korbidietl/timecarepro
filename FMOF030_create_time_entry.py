8ef6f01e5ea57159dcec4a26bf9796a38e55e902
Stella Muratore <stella.muratore@gmx.at>
2024-01-07 03:20:04 +0100

    Stella: Überarbeitung FMOF030

--- FMOF030_create_time_entry.py
+++ FMOF030_create_time_entry.py
@@ -1,5 +1,4 @@
 import base64
-import time
 
 from flask import Blueprint, request, redirect, url_for, render_template, flash, session
 from db_query import add_zeiteintrag, add_fahrt, check_for_overlapping_zeiteintrag, check_month_booked, client_dropdown
@@ -8,22 +7,23 @@ from datetime import datetime
 create_time_entry_blueprint = Blueprint('/create_time_entry', __name__)
 
 
-def check_time_entry_constraints(datum, startZeit, endZeit, klientID):
+def check_time_entry_constraints(datum, start_zeit, end_zeit, klient_id):
     # Prüft ob, Startzeitpunkt vor Endzeitpunkt liegt.
     jetzt = datetime.now()
-    if startZeit >= endZeit:
+    if start_zeit >= end_zeit:
         flash("Endzeitpunkt muss nach Startzeitpunkt sein.")
         return render_template("FMOF030_create_time_entry.html")
 
     # prüft ob startzeitpunkt in der zukunft liegt
-    if startZeit.time() > jetzt.time() or datum.date() > jetzt.date():
-        flash("Strtzeitpunkt muss in der Vergangenheit liegen")
+    if start_zeit.time() > jetzt.time() or datum.date() > jetzt.date():
+        flash("Startzeitpunkt muss in der Vergangenheit liegen")
         return render_template("FMOF030_create_time_entry.html")
 
     # prüft ob dieser monat schon gebucht wurde
     datum.strftime("%m.%Y")
-    if check_month_booked(datum, klientID):
-        flash("Die Stundennachweise für diesen Monat wurden bereits gebucht, es kann kein Eintrag mehr hinzugefügt werden")
+    if check_month_booked(datum, klient_id):
+        flash("Die Stundennachweise für diesen Monat wurden bereits gebucht, es kann kein Eintrag mehr hinzugefügt "
+              "werden")
         return render_template("FMOF030_create_time_entry.html")
 
 
@@ -51,26 +51,40 @@ def submit_arbeitsstunden():
         datum = request.form.get('datum')
         start_zeit = request.form.get('startZeit')
         end_zeit = request.form.get('endZeit')
-        fachkraft = request.form.get('fachkraft')
-        klient_id = request.form.get('klient')
+        fachkraft = "1" if request.form.get('fachkraft') is not None else "0"
+        klient_id = request.form.get('klientDropdown')
         beschreibung = request.form.get('beschreibung')
         interne_notiz = request.form.get('interneNotiz')
         unterschrift_klient = request.form.get('signatureDataKlient')
-        unterschrift_mitarbeiter = request.form.get('signatureDataKlient')
+        unterschrift_mitarbeiter = request.form.get('signatureDataMitarbeiter')
         absage = "1" if request.form.get('absage') is not None else "0"
 
+        # Überprüfung ob alle notwendigen Felder ausgefüllt wurden
+        required_fields = ['datum', 'startZeit', 'endZeit', 'klientDropdown', 'signatureDataMitarbeiter']
+
+        for field in required_fields:
+            if not request.form.get(field):
+                flash('Es müssen alle Felder ausgefüllt werden.')
+                return render_template('FMOF030_create_time_entry.html', klienten=klienten)
+
         # Konvertiere Datum und Uhrzeit in ein datetime-Objekt
-        datum_datetime = datetime.strptime(f"{datum}", '%Y-%m-%d')
-        start_datetime = datetime.strptime(f"{start_zeit}", '%H:%M')
-        end_datetime = datetime.strptime(f"{end_zeit}", '%H:%M')
+        datum_datetime = datetime.strptime(datum, '%Y-%m-%d')
+        start_zeit_datetime = datetime.strptime(start_zeit, '%H:%M').time()
+        end_zeit_datetime = datetime.strptime(end_zeit, '%H:%M').time()
+
+        start_datetime = datetime.combine(datum_datetime, start_zeit_datetime)
+        end_datetime = datetime.combine(datum_datetime, end_zeit_datetime)
 
         # Prüft ob, Startzeitpunkt vor Endzeitpunkt liegt.
         if not check_time_entry_constraints(datum_datetime, start_datetime, end_datetime, klient_id):
+
             # Umwandlung der Unterschriften
             if unterschrift_klient:
                 unterschrift_klient = base64_to_blob(unterschrift_klient)
+
             if unterschrift_mitarbeiter:
                 unterschrift_mitarbeiter = base64_to_blob(unterschrift_mitarbeiter)
+
 
             # Füge neuen Zeiteintrag hinzu und erhalte die ID
             zeiteintrag_id = add_zeiteintrag(unterschrift_mitarbeiter, unterschrift_klient, start_datetime,
@@ -80,24 +94,38 @@ def submit_arbeitsstunden():
             # Iteriere über alle Fahrt-Einträge und füge sie hinzu
             fahrt_index = 0
             while True:
-                abrechenbarkeit = request.form.get(f'abrechenbarkeit{fahrt_index}')
+                # Werte für jede Fahrt auslesen
+                abrechenbarkeit = request.form.get(f'abrechenbarkeit{fahrt_index}') == 'on'
                 start_adresse = request.form.get(f'start_adresse{fahrt_index}')
                 end_adresse = request.form.get(f'end_adresse{fahrt_index}')
                 kilometer = request.form.get(f'kilometer{fahrt_index}')
-                # Abbruchbedingung: Alle Felder sind None
-                if abrechenbarkeit is None and start_adresse is None and end_adresse is None:
-                    break
-                # Überprüfen ob alle erforderlichen Felder vorhanden sind
-                if not (kilometer and start_adresse and end_adresse):
-                    flash("Wenn eine Fahrt angelegt wird müssen alle Felder ausgefüllt sein")
-                add_fahrt(kilometer, start_adresse, end_adresse, abrechenbarkeit, zeiteintrag_id)
+
+                # Überprüfen, ob alle erforderlichen Felder ausgefüllt sind
+                if start_adresse and end_adresse and kilometer:
+                    # Konvertieren von Kilometern in einen numerischen Wert und überprüfen auf Null oder negativ
+                    try:
+                        kilometer = float(kilometer)
+                        if kilometer <= 0:
+                            continue  # Überspringt den aktuellen Eintrag, wenn Kilometer ungültig sind
+                    except ValueError:
+                        continue  # Überspringt den aktuellen Eintrag, wenn Kilometer keine gültige Zahl ist
+
+                    # Datenbankabruf, um die Fahrt hinzuzufügen
+                    add_fahrt(kilometer, start_adresse, end_adresse, abrechenbarkeit, zeiteintrag_id)
+
+                else:
+                    # Überprüfen, ob das Ende der Formulardaten erreicht ist
+                    if not start_adresse and not end_adresse and not kilometer:
+                        break  # Beendet die Schleife, wenn keine weiteren Fahrten vorhanden sind
+
                 fahrt_index += 1
 
             # prüft auf überschneidung einer bestehenden eintragung in der datenbank
             if check_for_overlapping_zeiteintrag(zeiteintrag_id, klient_id, start_datetime, end_datetime):
-                return redirect(url_for('/check_overlapping_time', zeiteintrag_id=zeiteintrag_id))
+                return redirect(url_for('check_overlapping_time.overlapping_time', zeiteintrag_id=zeiteintrag_id))
 
-        # Weiterleitung zurück zur Übersicht der abgelegten Stunden
-        return redirect(session.pop('url', None))
+        # Weiterleitung zurück zur Herkunftsfunktion
+        flash('Eintrag erfolgreich angelegt')
+        return redirect(return_url)
 
     return render_template('FMOF030_create_time_entry.html', klienten=klienten, return_url=return_url)
--- FS030_check_overlapping_time.py
+++ FS030_check_overlapping_time.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from flask import Blueprint, request, redirect, url_for, render_template
-from db_query import check_for_overlapping_zeiteintrag, get_zeiteintrag_by_id, get_zeiteintrag_with_fahrten_by_id
+from db_query import check_for_overlapping_zeiteintrag, get_zeiteintrag_by_id, get_zeiteintrag_with_fahrten_by_id, get_name_by_id
 
 check_overlapping_time_blueprint = Blueprint('check_overlapping_time', __name__)
 
@@ -19,9 +21,28 @@ def overlapping_time(zeiteintrag_id):
     overlapping_ids = check_for_overlapping_zeiteintrag(zeiteintrag_id, klient_id, startzeit, endzeit)
     overlapping_entries = []
 
-    for id in overlapping_ids:
-        entry_data = get_zeiteintrag_with_fahrten_by_id(id)
-        overlapping_entries.append(entry_data)
+    for ids in overlapping_ids:
+        entry_data = get_zeiteintrag_by_id(ids)
+        if entry_data:
 
-    return render_template('popup.html', overlapping_entries=overlapping_entries,
+            z_id = entry_data[0]
+            startzeit_str = entry_data[3]
+            endzeit_str = entry_data[4]
+            startzeit_obj = datetime.strptime(startzeit_str, '%Y-%m-%d %H:%M:%S')
+            endzeit_obj = datetime.strptime(endzeit_str, '%Y-%m-%d %H:%M:%S')
+
+            # Extrahieren der Uhrzeit als String
+            startzeit = startzeit_obj.strftime('%H:%M')
+            endzeit = endzeit_obj.strftime('%H:%M')
+            datum = startzeit_obj.strftime('%Y-%m-%d')
+
+            beschreibung = entry_data[8]
+            mitarbeiter_id = entry_data[5]
+            mitarbeiter = get_name_by_id(mitarbeiter_id)
+            client_id = entry_data[6]
+            client = get_name_by_id(client_id)
+
+            overlapping_entries.append((z_id, datum, startzeit,endzeit,beschreibung,mitarbeiter, client))
+
+    return render_template('FS030_check_overlapping_time.html', overlapping_entries=overlapping_entries,
                            original_zeiteintrag_id=zeiteintrag_id)
