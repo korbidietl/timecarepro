28c555a212e0d292cdfe4548e601c73a23dd85bb
korbiniandietl <kd.munchen@gmail.com>
2024-01-07 16:42:14 +0100

    Korbi: überarbeitung

--- FMOF060_delete_time_entry.py
+++ FMOF060_delete_time_entry.py
@@ -1,3 +1,6 @@
+import smtplib
+from email.mime.text import MIMEText
+
 from flask import Blueprint, request, flash, redirect, url_for, render_template, session
 from db_query import delete_zeiteintrag, check_booked
 
@@ -29,3 +32,24 @@ def delete_te(zeiteintrags_id):
             return redirect(session.pop('url', None))
     return render_template('FMOF060_delete_time_entry.html', zeiteintrags_id=zeiteintrags_id, return_url=return_url)
 
+
+
+def send_email(email, subject, body):
+    msg = MIMEText(body)
+
+    msg['Subject'] = subject
+    msg['From'] = 'deletetimeentry@timecarepro.de'
+    msg['To'] = email
+
+    with smtplib.SMTP('132.231.36.210', 1103) as smtp:
+        smtp.login('mailhog_grup3', 'Uni75Winfo17Master')
+        smtp.sendmail('deletetimeentry@timecarepro.de', [email], msg.as_string())
+
+
+def send_email_delete_time_entry(email,firstname, lastname, id):
+    subject = "Gelöschter Zeiteintrag"
+    body = (f"Sehr geehrte/r {firstname} {lastname}, \n\n"
+            f"Ihr Zeiteintrag {id} wurde von der Verwaltung gelöscht.\n\n"
+            f"Freundliche Grüße\n"
+            f"Ihr TimeCare Pro-Team")
+    send_email(email, subject, body)
