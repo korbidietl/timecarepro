6f67b8fbf2ae942a71c79b4728768f1df8925524
korbiniandietl <kd.munchen@gmail.com>
2024-01-07 15:29:33 +0100

    Korbi: überarbeitung

--- FGF030_redo_last_booking.py
+++ FGF030_redo_last_booking.py
@@ -9,10 +9,12 @@ redo_booking_blueprint = Blueprint('redo_booking_blueprint', __name__, template_
 def revidieren_buchung(client_id):
     last_buchung = get_last_buchung(client_id)
     if last_buchung:
-        last_buchung_id = int(last_buchung['id'])
+        last_buchung_id = int(last_buchung[1])  # Die ID ist das zweite Element des Tupels
         print(last_buchung_id)
+        last_buchung_month = last_buchung[0]  # Der Monat ist das erste Element des Tupels
+        print(last_buchung_month)
         delete_buchung(last_buchung_id)
-        flash(f"Buchung für {last_buchung} wurde erfolgreich revidiert.", 'success')
+        flash(f"Buchung für Monat {last_buchung_month} wurde erfolgreich revidiert.", 'success')
     else:
         flash("Keine Buchung gefunden, die revidiert werden könnte.", 'error')
     return redirect(session.pop('url', None))
