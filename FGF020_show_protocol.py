036b2fd40d647916d79416527cb056e451063664
korbiniandietl <kd.munchen@gmail.com>
2024-01-07 17:46:13 +0100

    Korbi: überarbeitung FGF020_show_protocol.py

--- FGF020_show_protocol.py
+++ FGF020_show_protocol.py
@@ -8,13 +8,13 @@ show_protocol_blueprint = Blueprint("show_protocol", __name__)
 def show_protocol():
 
     if request.method == 'POST':
+
+        nutzers = person_dropdown()
+
         von = request.form.get('von')
         bis = request.form.get('bis')
         aendernder_nutzer = request.form.get('nutzer')
         eintrags_id = request.form.get('eintrags_id')
-
-        nutzers = person_dropdown()
-        nutzer = {'nutzers': nutzers}
 
         protocols = get_protokoll(von, bis, aendernder_nutzer, eintrags_id)
 
@@ -23,11 +23,10 @@ def show_protocol():
             return render_template('FGF020_show_protocol.html',
                                    protocols=[], no_clients_message="Keine Protokolle vorhanden.")
 
-        return render_template('FGF020_show_protocol.html', protocols=protocols, **nutzer)
+        return render_template('FGF020_show_protocol.html', protocols=protocols, nutzers=nutzers)
 
     else:
         nutzers = person_dropdown()
-        nutzer = {'nutzers': nutzers}
-        return render_template('FGF020_show_protocol.html', protocols=[], **nutzer)
+        return render_template('FGF020_show_protocol.html', protocols=[], nutzers=nutzers)
 
 
