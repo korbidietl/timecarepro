e6ff961d6027a79288e5e2d81e3eda559f44e37d
korbiniandietl <kd.munchen@gmail.com>
2024-01-09 16:51:25 +0100

    Korbi: überarbeitung FAN040_show_client_table.py

--- FAN040_show_client_table.py
+++ FAN040_show_client_table.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, session, jsonify
-from db_query import get_client_table, get_client_table_sb
+from db_query import get_client_table, get_client_table_sb, check_month_booked
 from datetime import datetime
 
 show_clients_blueprint = Blueprint("show_clients", __name__)
@@ -13,8 +13,19 @@ def get_clients_data():
     month = request.args.get('monat')
     year = request.args.get('jahr')
 
+    month_int = int(month)
+    year_int = int(year)
+
+    datum = datetime(year_int, month_int, 1)
+
     if role == "Sachbearbeiter/Kostenträger":
         clients = get_client_table_sb(person, month, year)
+        for client in clients:
+            if not check_month_booked(datum, client[0]):
+                client[5] = (f"Noch kein Saldo vorhanden. Buchung zu Klient {client.vorname}, {client.nachname}"
+                             f"muss erst vorgenommen werden.")
+                client[6] = (f"Noch kein Saldo vorhanden. Buchung zu Klient {client.vorname}, {client.nachname}"
+                             f"muss erst vorgenommen werden.")
     else:
         clients = get_client_table(month, year)
 
